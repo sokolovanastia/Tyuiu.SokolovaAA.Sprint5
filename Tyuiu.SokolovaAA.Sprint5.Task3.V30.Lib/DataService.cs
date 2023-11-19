eb006fde201dc50ed98323a87b3ad7eaae3a2271
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-11-19 17:48:16 +0500

    Task3

---
+++ Tyuiu.SokolovaAA.Sprint5.Task3.V30.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SokolovaAA.Sprint5.Task3.V30.Lib
+{
+    public class DataService : ISprint5Task3V30
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            double z = ((Math.Pow(((double)x), 3)) - 1) / (4 * (Math.Pow(((double)x), 2)));
+            z = Math.Round(z, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(z));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.SokolovaAA.Sprint5.Task3.V30/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaAA.Sprint5.Task3.V30.Lib;
+namespace Tyuiu.SokolovaAA.Sprint5.Task3.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Соколова А.А. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнила: Соколова Анастасия Александровна | АСОиУБ-23-3               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до    *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
